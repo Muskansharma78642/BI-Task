645c43bebe167059f334cf3070df1f176ae266e3
Muskan Sharma <muskansharma78642@gmail.com>
2023-07-26 10:18:24 +0530

    sidebar setup

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,11 +6,12 @@ import Statistics from './Statistics'
 import WorldMap from './WorldMap'
 import LiveUpdate from './LiveUpdate'
 import Symptoms from './Symptoms'
+import VerticalNavbar from './VerticalNavbar'
 
 const Dashboard = () => {
   return (
     <div className='dashboardContainer' >
-        
+        <VerticalNavbar />
       Dashboard
       <Cases />
       <Death />
--- src/components/VerticalNavbar.js
+++ src/components/VerticalNavbar.js
@@ -1,10 +1,42 @@
-import React from 'react'
-
+import React from 'react';
+import {
+    CDBSidebar,
+    CDBSidebarContent,
+    CDBSidebarFooter,
+    CDBSidebarHeader,
+    CDBSidebarMenu,
+    CDBSidebarMenuItem,
+  } from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+  
 const VerticalNavbar = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='sidebar'>
+    <CDBSidebar textColor="#fff" backgroundColor="#4329c4" className='sidebar'>
+      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+        <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+          Sidebar
+        </a>
+      </CDBSidebarHeader>
+
+      <CDBSidebarContent className="sidebar-content">
+        <CDBSidebarMenu>
+          <NavLink exact to="/" activeClassName="activeClicked">
+            <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/tables" activeClassName="activeClicked">
+            <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/profile" activeClassName="activeClicked">
+            <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/analytics" activeClassName="activeClicked">
+            <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
+          </NavLink>
+        </CDBSidebarMenu>
+      </CDBSidebarContent>
+    </CDBSidebar>
+  </div>
   )
 }
 
