790ad790fdb8f5d6a8a7b98b3a94f2618bb377d0
Muskan Sharma <muskansharma78642@gmail.com>
2023-07-26 12:51:47 +0530

    Statictical Vertical bar graph completed

--- src/components/Cases.js
+++ src/components/Cases.js
@@ -2,37 +2,94 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import Chart from 'react-google-charts'
 import { Line } from 'react-chartjs-2';
-import Papa from 'papaparse'
+import Papa from 'papaparse';
 
 const URL = "https://data.covid19india.org/v4/min/data.min.json";
 
 const Cases = () => {
-    const [rows, setRows] = useState([])
-    
+    const [ cases, setCases ] = useState([]);
+    const [ loading, setLoading ] = useState(true)
+    const confirmedCases = [];
+
+    const getCasesData = async () => {
+        getCsvData()
+        // let res = await fetch('../../public/assets/case_time_series.csv');
+        // let data = await res.json();
+        // console.log('data --->', data);
+        // let setCaseData = [
+        //     data.AN, data.AP, data.AR, data.AS, data.BR, data.CH, data.CT, data.DL, data.DN, data.GA, data.GJ, data.HP, data.HR, data.JH, data.JK, data.KA, data.KL, data.LA, data.LD, data.MH, data.ML, data.MN, data.MP, data.MZ, data.NL, data.OR, data.PB, data.PY, data.RJ, data.SK, data.TG, data.TN, data.TR, data.TT, data.UP, data.UT, data.WB
+        // ]
+        // //console.log(setCaseData);
+        // setCases([...setCaseData])
+        // console.log('CASES',cases);
+        // extractConfirmedCases(cases)
+        // setLoading(false)
+    }
+
     useEffect(() => {
-      async function getData() {
-        const response = await fetch('../../public/assets/case_time_series.csv')
-        const reader = response.body.getReader()
-        const result = await reader.read() // raw array
-        console.log('raw array', result);
-        const decoder = new TextDecoder('utf-8')
-        const csv = decoder.decode(result.value) // the csv text
-        const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
-        const rows = results.data // array of objects
-        console.log('rows --->', rows );
-        setRows(rows)
+        getCsvData();
+    }, [loading])
+
+    const fetchCsv = async() => {
+        return fetch('../../public/assets/case_time_series.csv').then( async function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+
+            return reader.read().then(function (result) {
+                return decoder.decode(result.value);
+            });
+        });
+    }
+
+    const getData = (result) => {
+        setCases([result.data]);
+        console.log('case --->',cases)
+    }
+
+
+    const getCsvData = async () => {
+        let csvData = await fetchCsv();
+
+        Papa.parse(csvData, {
+            complete: getData
+        });
+    }
+
+
+
+    // const extractConfirmedCases = ( data ) => {
+    //     console.log('extractConfirmedCases')
+    //     console.log(data)
+    //     data.map( singleCase  => {
+    //         console.log(singleCase);
+    //         confirmedCases.push([singleCase.total.confirmed]);
+    //     })
+    //     console.log(confirmedCases);
+    // }
+
+    const caseChartOptions = {
+        hAxis: {
+          title: 'Time',
+        },
+        vAxis: {
+          title: 'Popularity',
+        },
+        series: {
+          1: { curveType: 'function' },
+        },
       }
-      getData()
-    }, []) // [] means just do this once, after initial render
-    return (
-      <div className="app">
-        <Chart
-            rows={rows}
-            chartType='Line'
-        />
-      </div>
-    )
-  
+
+  return (
+    <p>Cases</p>
+    // <Chart
+    //     width={'700px'}
+    //     height={'410px'}
+    //     chartType="LineChart"
+    //     loader={<div>Loading Chart</div>}
+    //     data={confirmedCases}
+    //     rootProps={{ 'data-testid': '2' }}
+    // />
+  )
 }
 
 export default Cases
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,86 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { Bar } from "react-chartjs-2";
+import { BarElement,  CategoryScale,Chart as ChartJS,Legend, LinearScale,Title, Tooltip } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement,Title,Tooltip,Legend);
+
+const URL = "https://data.covid19india.org/v4/min/data.min.json";
 
 const Statistics = () => {
+    const [loading, setLoading] = useState(true)
+    const [deathData, setDeathData] = useState([]);
+    const [recoverData, setRecoverData] = useState([]);
+    let deathCases = [];
+    let recoveredCases = [];
+
+    const fetchURL = async () => {
+        let res = await fetch(URL);
+        let data = await res.json()
+        let setCaseData = [
+            data.AN, data.AP, data.AR, data.AS, data.BR, data.CH, data.CT, data.DL, data.DN, data.GA, data.GJ, data.HP, data.HR, data.JH, data.JK, data.KA, data.KL, data.LA, data.LD, data.MH, data.ML, data.MN, data.MP, data.MZ, data.NL, data.OR, data.PB, data.PY, data.RJ, data.SK, data.TG, data.TN, data.TR, data.TT, data.UP, data.UT, data.WB
+        ]
+        console.log(setCaseData);
+        setCaseData.map(( singleCase ) => {
+            deathCases.push(singleCase.total.deceased);
+            recoveredCases.push(singleCase.total.recovered);
+        })
+        setDeathData([...deathCases]);
+        setRecoverData([...recoveredCases]);
+
+        console.log('death data -->',deathData)
+        console.log('recover data -->', recoverData)
+
+        
+        console.log('death -->',deathCases)
+        console.log('recover -->', recoveredCases)
+
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchURL()
+    }, [loading])
+
+    const option = {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+          },
+        },
+      };
+
+      let labels=["01", "02", "03", "04", "05", "06"]
+
+      
+      const data = {
+        labels,
+        datasets: [
+          {
+            label: "Death",
+            // data: [ 20, 30, 40, 50, 60,70],
+            data: deathData,
+            backgroundColor: "red",
+          },
+          {
+            label:'Recover',
+            // data:[15,20,25,40,45,60],
+            data: recoverData,
+            backgroundColor:'green'
+          },
+      
+        ],
+      
+      };
+      
+
   return (
-    <div>
-      Stats
+    <div className='statistics'>
+      <p style={{ padding: '15px' }}><b>Covid-19 Statistics</b></p>  
+      <div className='statisticsBarGraph'>
+        <Bar options={option} data={data} />
+      </div>
     </div>
   )
 }
